9a5dac1756909ff74ffb889c67b215761069429d
Nduwayo Fabrice <nduwayof@outlook.com>
2019-07-24 20:50:36 -0500

    Minor changes

--- src/main/java/hcmc/hcmc/controllers/PatientRestController.java
+++ src/main/java/hcmc/hcmc/controllers/PatientRestController.java
@@ -2,7 +2,6 @@ package hcmc.hcmc.controllers;
 
 import hcmc.hcmc.models.Patient;
 import hcmc.hcmc.services.IPatientQueryService;
-import hcmc.hcmc.services.IPatientService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
